83481dd3f2f402fc95eab5802a6fd1fe3e754439
Kris <kris_yeh@himax.com.tw>
2023-12-22 14:42:32 +0800

    delete middleware folder

--- EPII_CM55M_APP_S/middleware/cli/dummy_menu.h
+++
@@ -1,8 +0,0 @@
-BEGIN_MENU (root)
-ADD_CMD(dummy, cli_menu_not_defined, "This is to fail the linking")
-END_MENU
-
-BEGIN_MENU(global)
-ADD_CMD(dummy, cli_menu_not_defined, "This is to fail the linking")
-END_MENU
-
